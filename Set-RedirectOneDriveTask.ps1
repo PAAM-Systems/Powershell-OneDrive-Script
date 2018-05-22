2ffd1a085d6a678c4c1664ea8480a06498829a96
Martin Sidén <martin.siden@paam.se>
2018-05-22 10:44:15 +0200

    Tweaks

--- Set-RedirectOneDriveTask.ps1
+++ Set-RedirectOneDriveTask.ps1
@@ -3,26 +3,38 @@
     Creates a scheduled task to enable folder redirection into OneDrive
 #>
 
-# Variables
+# Define some variables that we need acccess to
 $Url = "https://raw.githubusercontent.com/PAAM-Systems/Powershell-OneDrive-Script/master/Redirect-FoldersOneDrive.ps1"
 $Target = "$env:ProgramData\Scripts"
 $Script = "Redirect-FoldersOneDrive.ps1"
+$Domain = "$env:UserDomain"
+$UserName = "$env:UserName"
 
 Start-Transcript -Path "$Target\Set-RedirectOneDriveTask-ps1.log"
 
 # If local path for script doesn't exist, create it
 If (!(Test-Path $Target)) { New-Item -Path $Target -Type Directory -Force }
 
-# Download the script from the source repository
+# Download the real script from GitHub
 If (Test-Path "$Target\$Script") { Remove-Item -Path "$Target\$Script" -Force }
 Start-BitsTransfer -Source $Url -Destination "$Target\$Script"
 
-# Create the scheduled task to run the script at logon
+# Create a scheduled task action (make sure to bypass execution policy)
 $action = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument "-ExecutionPolicy Bypass -NonInteractive -WindowStyle Hidden -File $Target\$Script"
+
+# Define the trigger object for the scheduled task (run at user login with random delay)
 $trigger =  New-ScheduledTaskTrigger -AtLogon -RandomDelay (New-TimeSpan -Minutes 1)
+
+# Define some settings for it
 $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -Hidden -DontStopIfGoingOnBatteries -Compatibility Win8
-$principal = New-ScheduledTaskPrincipal -GroupId "BUILTIN\Users"
+
+# Define what user account that should execute the scheduled task
+$principal = New-ScheduledTaskPrincipal "$Domain\$UserName"
+
+# Create the scheduled task
 $task = New-ScheduledTask -Action $action -Trigger $trigger -Settings $settings -Principal $principal
+
+# Finally, register the scheduled task in Windows
 Register-ScheduledTask -InputObject $task -TaskName "Redirect Folders to OneDrive"
 
 Stop-Transcript
